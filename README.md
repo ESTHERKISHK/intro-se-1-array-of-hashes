caf02d04c38c270f61df831cf60aa273b60694c2
jolson615 <jolson615@gmail.com>
2017-08-02 16:08:24 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# intro-se-1-array-of-hashes
+# Survey Says!
+
+## The Goal
+
+The information in the database file attached here has results from a survey Jeff took of some of his friends. These are real people, and real people don't always provide the easiest data to work with.
+
+In the challenge.rb file, Jeff has written some questions he has. Use your knowledge of arrays and hashes to get at the information asked for in each of the 9 challenges.
+
+## Getting Started
+
+If you already have a good idea of how to start this based on previous labs, go for it!
+
+If you're struggling to get started, try to get a feel for the shape of the data by adding the three puts statements below to the challenge.rb file:
+
+```Ruby
+require_relative 'database.rb'
+
+# All the survey responses are stored in an array called "people".
+puts people
+puts "THE FIRST PERSON IS:"
+puts people[1]
+# 1. Print out the name of the first person who responded to the survey
+```
+
+Then run the code in the console:
+
+```Bash
+ruby challenge.rb
+```
---
+++ challenge.rb
@@ -0,0 +1,55 @@
+require_relative 'database.rb'
+
+# All the survey responses are stored in an array called "people".
+
+
+
+# 1. Print out the name of the first person who responded to the survey
+
+
+
+
+# 2. Print out Brenton's second favorite band
+
+
+
+
+# 2. For each person on this list, print out a descriptor of where they live in the form "____ lives in _____."
+
+
+
+
+# 3. Print out each person's nickname. Write some control flow that determines whether / how to respond if someone didn't provide a nickname.
+
+
+
+
+# 4. Find the average of the respondents' ages.
+
+
+
+
+# 5. Print out the percentage of respondents who have brown hair.
+
+
+
+
+# 6. Create two new arrays - one for programmers, and one for non programmers.
+#    Sort all the people in the contacts array into these two more specific arrays.
+
+
+
+
+# 7. Iterate over the array in order to figure out how many respondents listed "The Office" as their one of their favorite shows.
+
+
+
+
+# 8. Print out a list of all the bands that are liked by at least two people.
+#    Pro-tip: some respondents capitalized band names and other respondents did not, so for example, if Panic at the Disco isn't listed, your code isn't accurately reporting your results.
+
+
+
+
+
+# 9. Identify anyone on the list who has no common interests (bands or shows) with anyone else.
